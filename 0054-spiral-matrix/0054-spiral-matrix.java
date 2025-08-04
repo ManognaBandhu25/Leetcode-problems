5495bea672bf7444bccbcc063ccbaa43da75c5e6
Manogna Bandhu <manognabandhu25@gmail.com>
2025-08-04 10:56:58 +0530

    Time: 0 ms (100%), Space: 41.8 MB (26.13%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -6,28 +6,28 @@ class Solution {
         int bottom = matrix.length - 1;
         int right = matrix[0].length - 1;
         while(top<=bottom && left<=right){
-            for(int i = left;i<=right;i++) {
+            for(int i = left;i<=right;i++) 
                 res.add(matrix[top][i]);
-            }
+            
             top++;
 
-            for(int i = top;i<=bottom;i++){
+            for(int i = top;i<=bottom;i++)
                 res.add(matrix[i][right]);
-            }
+            
             right--;
 
             if (top <= bottom) {
-            for (int i = right; i >= left; i--) {
+            for (int i = right; i >= left; i--) 
                 res.add(matrix[bottom][i]);
             }
             bottom--;
-            }
+            
             if(left<=right){
-                for(int i = bottom;i>=top;i-- ){
+                for(int i = bottom;i>=top;i-- )
                     res.add(matrix[i][left]);
                 }
                 left++;
-            }
+            
         }
         return res;
     }
