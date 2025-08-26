c36f70a53545620414faa613bd0ae88e4eb34221
Manogna Bandhu <manognabandhu25@gmail.com>
2025-08-26 10:58:01 +0530

    Time: 895 ms (84.32%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -1,5 +1,5 @@
 select r.contest_id,
-ROUND(COUNT(distinct r.user_id) * 100.0 / (select count(*) from Users), 2) AS percentage
+ROUND(COUNT(distinct r.user_id) * 100 / (select count(*) from Users), 2) AS percentage
 from Register r
 group by r.contest_id
 order by percentage desc,r.contest_id;
